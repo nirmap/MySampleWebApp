0f105d969c3d61cf75f3e2425b8a2454088d9fbe
nirmap <57590624+nirmap@users.noreply.github.com>
2020-12-22 19:44:38 +0530

    Update SeleniumExampleIT.java

--- src/test/java/com/mycompany/app/SeleniumExampleIT.java
+++ src/test/java/com/mycompany/app/SeleniumExampleIT.java
@@ -10,28 +10,27 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  * Selenium test for simple App.
  */
 public class SeleniumExampleIT
-{   
+{
     WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver();
-	
+    private WebDriver driver = new ChromeDriver();
+
     @Test
     public void googleCheeseExample() throws Exception {
-       
-       
-      
-       
+
+
+
+
         // And now use this to visit Google
         driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
 
-     
+
+
 
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
 
-       
-       
+
+
     }
 
 
