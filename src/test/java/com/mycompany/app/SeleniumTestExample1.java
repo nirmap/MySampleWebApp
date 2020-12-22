65926e772a382495056788d8374429e00ae68e76
nirmap <57590624+nirmap@users.noreply.github.com>
2020-12-22 13:56:05 +0530

    Update SeleniumTestExample1.java

--- src/test/java/com/mycompany/app/SeleniumTestExample1.java
+++ src/test/java/com/mycompany/app/SeleniumTestExample1.java
@@ -1,47 +1,33 @@
 package com.mycompany.app;
 
-import com.lazerycode.selenium.DriverBase;
-import com.lazerycode.selenium.page_objects.GoogleHomePage;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 /**
  * Selenium test for simple App.
  */
-public class SeleniumTestExample1 extends DriverBase
+public class SeleniumTestExample1
 {   
-   private ExpectedCondition<Boolean> pageTitleStartsWith(final String searchString) {
-        return driver -> driver.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
-    }
+   WebDriver driver = new ChromeDriver();
+    
 
     @Test
     public void googleCheeseExample() throws Exception {
-        // Create a new WebDriver instance
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
-        WebDriver driver = getDriver();
-
+       
         // And now use this to visit Google
         driver.get("http://www.google.com");
         // Alternatively the same thing can be done like this
         // driver.navigate().to("http://www.google.com");
 
-        GoogleHomePage googleHomePage = new GoogleHomePage();
+     
 
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
 
-        googleHomePage.enterSearchTerm("Cheese").submitSearch();
-
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        WebDriverWait wait = new WebDriverWait(driver, 10, 100);
-        wait.until(pageTitleStartsWith("Cheese"));
-
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
+       
+       
     }
 
 
