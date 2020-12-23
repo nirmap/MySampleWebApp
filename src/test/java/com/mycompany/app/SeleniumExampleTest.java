4be1571f8c23661cf7bd47ef2616601ea5d9f9bd
nirmap <57590624+nirmap@users.noreply.github.com>
2020-12-23 10:34:31 +0530

    Update SeleniumExampleTest.java

--- src/test/java/com/mycompany/app/SeleniumExampleTest.java
+++ src/test/java/com/mycompany/app/SeleniumExampleTest.java
@@ -1,33 +1,47 @@
 package com.mycompany.app;
 
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * Selenium test for simple App.
  */
 public class SeleniumExampleTest
-{   
-   WebDriver driver = new ChromeDriver();
-    //testng
+{
+    //spec char
+    
+   
 
     @Test
     public void googleCheeseExample() throws Exception {
-       
+
+        System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/ySampleWebApp_bitwiseman-patch-1/src/test/resources/webdriver/binaries/linux/googlechrome/64bit/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("no-sandbox");
+        // Must maximize Chrome by `start-maximized`
+        options.addArguments("start-maximized");
+        options.addArguments("disable-gpu");
+        options.addArguments("window-size=1420,1080");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+        
+
         // And now use this to visit Google
         driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
 
-     
+
+
 
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
+        
+       
 
-       
-       
+
     }
 
 
